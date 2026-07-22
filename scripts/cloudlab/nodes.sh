2f775cbf1d6b32c96405fe173dff338fa6cb8a4a
Yuhang Song <i@syh.one>
2026-07-22 04:36:57 +0000

    add new nodes

--- scripts/cloudlab/nodes.sh
+++ scripts/cloudlab/nodes.sh
@@ -5,9 +5,9 @@
 # config.json (10.0.0.101 == node-0, etc.). Update this list each time you
 # swap CloudLab experiments.
 NODES=(
-	"apt186.apt.emulab.net"   # node-0 / 10.0.0.101  (control-plane)
-	"apt184.apt.emulab.net"   # node-1 / 10.0.0.102
-	"apt179.apt.emulab.net"   # node-2 / 10.0.0.103
-	"apt190.apt.emulab.net"   # node-3 / 10.0.0.104
-	"apt183.apt.emulab.net"   # node-4 / 10.0.0.105
+	"apt130.apt.emulab.net"   # node-0 / 10.0.0.101  (control-plane)
+	"apt136.apt.emulab.net"   # node-1 / 10.0.0.102
+	"apt131.apt.emulab.net"   # node-2 / 10.0.0.103
+	"apt129.apt.emulab.net"   # node-3 / 10.0.0.104
+	"apt132.apt.emulab.net"   # node-4 / 10.0.0.105
 )
